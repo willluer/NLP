2f97b7e2b7b4fd4b6952290b9185b3fddf8a44d1
Will <Owner-PC@owners-mbp.home>
2017-10-15 12:44:18 -0500

    Responds to query

--- text_similarity.py
+++ text_similarity.py
@@ -1,17 +1,21 @@
 #do text similarities between clusters
 import gensim
 import nltk
+import numpy as np
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cluster import DBSCAN
 from nltk.corpus import gutenberg
 
+lemmatizer = WordNetLemmatizer()
 sample = gutenberg.raw("bible-kjv.txt")
-ex = sent_tokenize(sample)
-ex = ex[:100]
+stopWords = set(stopwords.words('english'))
 
-#---------------------------------------------------------
+ex = sent_tokenize(sample)[:300]
+
+#--------------      CLUSTERING      ----------------------
 vectorizer = TfidfVectorizer(stop_words='english')
 X = vectorizer.fit_transform(ex)
 
@@ -22,58 +26,86 @@ labels = model_dbscan.labels_
 # Number of clusters in labels, ignoring noise if present.
 n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
 print('Estimated number of DBSCAN clusters: %d' % n_clusters_)
-print("\n")
-#print("Prediction")
 
 unique_labels = set(labels)
 cluster_list = list(unique_labels)
 
 #ORGANIZE SENTENCES
-#Create list of lists where each list represents a different cluster
+#Create list of lists where each list represents a different cluster's sentences
+# [ [ cluster 1 sentences]
+#   [ cluster 2 sentences]
+#   ....
+#   [ cluster n senetences] ]
+
 group_label = [[] for x in xrange(len(unique_labels)-1)]
 
 noise_count = 0
 for i in range(len(ex)):
     if labels[i] == -1:
         noise_count = noise_count + 1
-        #To print the 'noise', uncomment line below
-        #print ex[i]
+        #does not add noise to a cluster group
         continue
+
+    #appends the current sentence to the appropriate location
+    #group_label is the list of lists
+    #cluster_list.index(labels[i]) gets the location where sentence should be added
     group_label[cluster_list.index(labels[i])].append(ex[i])
 
-tok_groups = [[] for x in xrange(len(group_label))]
+
+tok_group_label = [[] for x in xrange(len(group_label))]
 
 for i in range(len(group_label)):
-    tok_groups[i] = [[w.lower() for w in word_tokenize(text)] for text in group_label[i]]
+    #tokenize each sentence in each cluster group, maintaining sentence order
+    #filter out english stop words
+    tok_group_label[i] = [[lemmatizer.lemmatize(w.lower()) for w in word_tokenize(text) if w not in stopWords] for text in group_label[i]]
 
-main_dict = [item for sublist in tok_groups for item in sublist]
+#each sentence in the cluster is aggregated into a single array
+cluster_grouping = [[] for x in xrange(len(group_label))]
+for i in range(len(tok_group_label)):
+    for j in range(len(tok_group_label[i])):
+        cluster_grouping[i] = cluster_grouping[i] + tok_group_label[i][j]
+
+#main_dict - dictionary of every word analyzed
+main_dict = [item for sublist in tok_group_label for item in sublist]
 main_dict = gensim.corpora.Dictionary(main_dict)
 
-corpus = []
-for i in range(len(tok_groups)):
-    corpus.append([main_dict.doc2bow(entry) for entry in tok_groups[i]])
-#print(corpus)
+#Create corpus
+#maps each word and word frequency to the dictionary
+#corpus = []
+corpus = [main_dict.doc2bow(cluster_group) for cluster_group in cluster_grouping]
 
 #print(main_dict.token2id['god'])
 print("Number of words in dictionary:",len(main_dict))
 #for i in range(100):
 #    print(i, main_dict[i])
 
-tf_idf = gensim.models.TfidfModel(corpus[0])
-#print(tf_idf)
+tf_idf = gensim.models.TfidfModel(corpus)
+sims = gensim.similarities.Similarity('/Users/Owner-PC/Documents/Coding/NLP/workdir/',tf_idf[corpus],num_features=len(main_dict))
+
 s = 0
 for i in corpus:
     s += len(i)
-#print(s)
+print(s)
 
-sims = gensim.similarities.Similarity('/usr/workdir/',tf_idf[corpus[0]],num_features=len(main_dict))
-print("corpus[0] = ", corpus[0])
-print(sims)
-print(type(sims))
+#for i in range(len(corpus)):
+#    sims.append(gensim.similarities.Similarity('/usr/workdir/',tf_idf[i][corpus[i]],num_features=len(main_dict)))
 
-query_doc = [w.lower() for w in word_tokenize("The Above above above above and light from the sun is from god")]
-print(query_doc)
+#print(sims)
+#print(type(sims))
+
+np.set_printoptions(precision=3, suppress=True)
+query_doc = [lemmatizer.lemmatize(w.lower()) for w in word_tokenize("The Above light Lights lights above above above and light from the sun gathered waters 1:9 fowl abundantly is from god") if w.lower() not in stopWords]
+#print(query_doc)
 query_doc_bow = main_dict.doc2bow(query_doc)
-print(query_doc_bow)
+#print(query_doc_bow)
 query_doc_tf_idf = tf_idf[query_doc_bow]
 print(query_doc_tf_idf)
+sim_calculation = sims[query_doc_tf_idf]
+print(sim_calculation)
+
+query_doc = [lemmatizer.lemmatize(w.lower()) for w in word_tokenize("2:12 As an offering of first-fruits ye may bring them unto the LORD; but they shall not come up for a sweet savour on the altar. 2:13 And every meal-offering of thine shalt thou season with salt; neither shalt thou suffer the salt of the covenant of thy God to be lacking from thy meal-offering; with all thy offerings thou shalt offer salt") if w.lower() not in stopWords]
+query_doc_bow = main_dict.doc2bow(query_doc)
+query_doc_tf_idf = tf_idf[query_doc_bow]
+sim_calculation = sims[query_doc_tf_idf]
+
+print(sim_calculation)
--- tokenize_cluster.py
+++ tokenize_cluster.py
@@ -1,3 +1,5 @@
+#do text similarities between clusters
+
 import nltk
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.stem import WordNetLemmatizer
@@ -5,89 +7,56 @@ from nltk.stem import WordNetLemmatizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cluster import KMeans, DBSCAN
 from sklearn.metrics import adjusted_rand_score
+from nltk.corpus import gutenberg
 
-lemmatizer = WordNetLemmatizer()
+sample = gutenberg.raw("bible-kjv.txt")
 
-sun_dict = {'sunshine','sun','bright','hot', 'shine'}
-rain_dict = {'rain', 'shower','sunshower', 'storm', 'rains'}
-word_dict = sun_dict | rain_dict
+ex = sent_tokenize(sample)
 
-ex = list(range(6))
-token = list(range(6))
-
-ex = ["the sunshine was strong and hot",
-    "the hot sun is yellow",
-    "i like sunshine",
-    "There is rain in the storm",
-    "rain shower during the day",
-    "sunshower rain rain sun storm",
-    "When it rain it pour",
-    "I listen to rain drops during sunshower",
-    "sun sun sun shine",
-    "rain rain rain rain rain rain",
-    "storm means to rain",
-    "The dog is cute",
-    "I play with the dog in the rain and the sun",
-    "My cat and dog are not friends",
-    "Rain is the name of my dog",
-    "Cats and dogs do not get along",
-    "Fetch dog",
-    "Sun and rain and dog",
-    "Go get a new dog",
-    "Why is there a cloud during the sun",
-    "Do not rain shower on me with my dog"
-    "cat dog cat dog rain sun",
-    "Cute dogs have a higher likability",
-    "My dog name is hank",
-    "Hello put on your sun screen unless there is rain"]
-
-test = "It is going to rain and storm"
-
-for i in range(6):
-    token[i] = nltk.word_tokenize(ex[i])
-
-print(token)
-print()
-print()
+ex = ex[:1000]
 
 #---------------------------------------------------------
 vectorizer = TfidfVectorizer(stop_words='english')
 X = vectorizer.fit_transform(ex)
 
 true_k = 3
-model_kmeans = KMeans(n_clusters=true_k, init='k-means++', max_iter=100, n_init=1)
-model_kmeans.fit(X)
+#model_kmeans = KMeans(n_clusters=true_k, init='k-means++', max_iter=100, n_init=1)
+#model_kmeans.fit(X)
 
-print("Top terms per cluster:")
-order_centroids = model_kmeans.cluster_centers_.argsort()[:, ::-1]
-terms = vectorizer.get_feature_names()
-for i in range(true_k):
-    print("Cluster %d:" % i),
-    for ind in order_centroids[i, :10]:
-        print(' %s' % terms[ind]),
-    print
+#print("Top terms per cluster:")
+#order_centroids = model_kmeans.cluster_centers_.argsort()[:, ::-1]
+#terms = vectorizer.get_feature_names()
+#for i in range(true_k):
+#    print("Cluster %d:" % i),
+#    for ind in order_centroids[i, :10]:
+#        print(' %s' % terms[ind]),
+#    print
 
 
-model_dbscan = DBSCAN(eps = 1.02, min_samples = 2).fit(X)
+model_dbscan = DBSCAN(eps = 1.1, min_samples = 2).fit(X)
 
 labels = model_dbscan.labels_
 # Number of clusters in labels, ignoring noise if present.
 n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
 print('Estimated number of DBSCAN clusters: %d' % n_clusters_)
-print('labels:')
-print(labels)
 print("\n")
-print(model_dbscan.components_)
-print("Prediction")
+#print("Prediction")
+unique_labels = set(labels)
+group_0 = []
+for i in range(len(ex)):
+    if labels[i] == 0:
+        group_0.append(ex[i])
 
-Y = vectorizer.transform(["storm storm storm storm"])
-prediction = model_kmeans.predict(Y)
-print(prediction)
+#group_0 = [w for w in ex if w in labels is 0]
+print(group_0)
+Y = vectorizer.transform(["rain"])
+#prediction = model_kmeans.predict(Y)
+#print(prediction)
 prediction = model_dbscan.fit_predict(Y)
 print(prediction)
 
-Y = vectorizer.transform(["sun sun sun sun sun sun"])
-prediction = model_kmeans.predict(Y)
-print(prediction)
+Y = vectorizer.transform(["Let the sun makith light for the people"])
+#prediction = model_kmeans.predict(Y)
+#print(prediction)
 prediction = model_dbscan.fit_predict(Y)
 print(prediction)
